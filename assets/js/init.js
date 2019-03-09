bf34d08796695f608547111c0d31e118df939ef9
Nick Pateman <nick@devoctomy.com>
2019-03-09 14:12:05 +0000

    Initial commit after getting theme to an initial state

---
+++ assets/js/init.js
@@ -0,0 +1,56 @@
+skel.init(
+	{
+		prefix: '/assets/css/style',
+		resetCSS: true,
+		boxModel: 'border',
+		grid:
+		{
+			gutters: 50
+		},
+		breakpoints:
+		{
+			'mobile':
+			{
+				range: '-520',
+				lockViewport: true,
+				containers: 'fluid',
+				grid: {
+					collapse: true,
+					gutters: 10
+				}
+			},
+			'desktop':
+			{
+				range: '520-',
+				containers: 720
+			}
+		}
+	},
+	{
+		panels:
+		{
+			panels:
+			{
+				navPanel:
+				{
+					breakpoints: 'mobile',
+					position: 'left',
+					style: 'reveal',
+					size: '80%',
+					html: '<div data-action="navList" data-args="nav"></div>'
+				}
+			},
+			overlays:
+			{
+				titleBar:
+				{
+					breakpoints: 'mobile',
+					position: 'top-left',
+					height: 44,
+					width: '100%',
+					html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span><span class="title" data-action="" data-args="nav"></span>'
+				}
+			}
+		}
+	}
+);
